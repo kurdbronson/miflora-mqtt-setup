a60bf32164cb99e7c5a17771cdf61d57ce3f1137
kurdbronson <kenan.erden95@googlemail.com>
2025-05-13 12:24:04 +0100

    ble_scan.py script for reading BLE device

---
+++ ble_scan.py
@@ -0,0 +1,10 @@
+import asyncio
+from bleak import BleakScanner
+
+async def scan():
+    devices = await BleakScanner.discover(timeout=5.0)
+    for d in devices:
+        print(f"{d.address} — {d.name}")
+
+if __name__ == "__main__":
+    asyncio.run(scan())
